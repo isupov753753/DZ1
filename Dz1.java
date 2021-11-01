dab1c038b77ec3d50a5af7357a2550f1b591091c
isupov753753 <isupov_aleksei@mail.ru>
2021-11-01 23:16:40 +0300

    Create Dz1.java

---
+++ Dz1.java
@@ -0,0 +1,6 @@
+public class Dz1 {
+    public static void main (String [] args) {
+        System.out.println("Где я могу найти запись первого занятия?");
+        System.out.print("Где я могу найти ссылку на телеграмм канал?");
+    }
+}
